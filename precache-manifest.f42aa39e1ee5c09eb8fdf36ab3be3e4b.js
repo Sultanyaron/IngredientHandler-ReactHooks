6b9be0de02d4c675330ae0d88e46cbf8b60ab04c
Sultanyaron <sultanyaron@gmail.com>
2019-10-23 17:26:32 +0300

    Updates

---
+++ precache-manifest.f42aa39e1ee5c09eb8fdf36ab3be3e4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74ed0df80801d0c774edbcf90fbcb71",
+    "url": "/IngredientHandler-ReactHooks/index.html"
+  },
+  {
+    "revision": "68c22311d29e570a07a9",
+    "url": "/IngredientHandler-ReactHooks/static/css/main.5c3ba4df.chunk.css"
+  },
+  {
+    "revision": "da6e7d3cf528ab3044d7",
+    "url": "/IngredientHandler-ReactHooks/static/js/2.fd64e6a3.chunk.js"
+  },
+  {
+    "revision": "68c22311d29e570a07a9",
+    "url": "/IngredientHandler-ReactHooks/static/js/main.f1cb0986.chunk.js"
+  },
+  {
+    "revision": "2382bc580cf5892ac94c",
+    "url": "/IngredientHandler-ReactHooks/static/js/runtime~main.a29dfd57.js"
+  }
+]);
